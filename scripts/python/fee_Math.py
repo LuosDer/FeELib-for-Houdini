999ef9654e4c29b0668807949f11585129306145
五行精灵 <fiveelementself@163.com>
2021-11-24 00:37:40 +0800

    debug

--- scripts/python/fee_Math.py
+++ scripts/python/fee_Math.py
@@ -3,10 +3,23 @@
 # from importlib import reload
 # reload(fee_Math)
 
-def isNumber(input):
+def isNumber(strInput):
     try:
-        float(input)
+        float(strInput)
         return True
     except ValueError:
         pass
     return False
+
+
+def valueType(strInput):
+    try:
+        float(strInput)
+        if isinstance(eval(strInput), int):
+            return 0
+        else:
+            return 1
+    except ValueError:
+        pass
+
+    return 2
