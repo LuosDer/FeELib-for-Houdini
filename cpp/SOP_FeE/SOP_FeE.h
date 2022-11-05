c8ddeef4e5a8d01722a4423aea8f43ab92624e14
五行精灵 <32925649+Fe-Elf@users.noreply.github.com>
2022-11-05 17:22:22 +0800

    debug

---
+++ cpp/SOP_FeE/SOP_FeE.h
@@ -0,0 +1,45 @@
+
+#ifndef __SOP_FeE_h__
+#define __SOP_FeE_h__
+
+#include <SOP/SOP_Node.h>
+#include <UT/UT_StringHolder.h>
+
+namespace SOP_FeE_Namespace {
+/// This is the SOP class definition.  It doesn't need to be in a separate
+/// file like this.  This is just an example of a header file, in case
+/// another file needs to reference something in here.
+/// You shouldn't have to change anything in here except the name of the class.
+class SOP_FeE : public SOP_Node
+{
+public:
+    static PRM_Template *buildTemplates();
+    static OP_Node *myConstructor(OP_Network *net, const char *name, OP_Operator *op)
+    {
+        return new SOP_FeE(net, name, op);
+    }
+
+    static const UT_StringHolder theSOPTypeName;
+    
+    const SOP_NodeVerb *cookVerb() const override;
+
+protected:
+    SOP_FeE(OP_Network *net, const char *name, OP_Operator *op)
+        : SOP_Node(net, name, op)
+    {
+        // All verb SOPs must manage data IDs, to track what's changed
+        // from cook to cook.
+        mySopFlags.setManagesDataIDs(true);
+    }
+    
+    ~SOP_FeE() override {}
+
+    /// Since this SOP implements a verb, cookMySop just delegates to the verb.
+    OP_ERROR cookMySop(OP_Context &context) override
+    {
+        return cookMyselfAsVerb(context);
+    }
+};
+} // End SOP_FeE_Namespace namespace
+
+#endif
