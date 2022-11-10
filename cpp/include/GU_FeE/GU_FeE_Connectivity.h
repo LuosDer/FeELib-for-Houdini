c7d45262b40712fc833687f14b40b3f3b48d66fa
五行精灵 <32925649+Fe-Elf@users.noreply.github.com>
2022-11-11 03:21:15 +0800

    debug

--- cpp/include/GU_FeE/GU_FeE_Connectivity.h
+++ cpp/include/GU_FeE/GU_FeE_Connectivity.h
@@ -85,24 +85,78 @@ connectivity(
     //    }
     //}
 
-
+    using timeTtype = std::milli;
     //time_t timeTotal, timeSub, timeStart, timeEnd;
 
     //time(&timeTotal);
-    long long timeTotal = 0;
+    double timeTotal = 0;
     //_CHRONO time_point<steady_clock> timeTotal = 0;
     GEO_Detail::GEO_EdgeAdjArray adjElems;
-    GA_Offset attribValue;
-    GA_Offset elemHeapLast;
     GA_Offset adjElem;
-    GA_Size	numAdj;
 #if 1
     UT_ValArray<GA_Offset> elemHeap;
 #else
     //UT_Array<GA_Offset> elemHeap;
 #endif
+
+
+
+#if 1
+    geo->forEachPrimitive([&geo, &attribHandle, &classnum, &adjElems, &adjElem, &timeTotal, &elemHeap](const GA_Offset& elemoff)
+    {
+        auto start = std::chrono::steady_clock::now();
+        auto end = std::chrono::steady_clock::now();
+        start = std::chrono::steady_clock::now();
+        GA_Offset attribValue = attribHandle.get(elemoff);
+        if (attribValue != UNREACHED_NUMBER)
+            return;
+        //if (elemHeap.size() == 0)
+        //elemHeap.append(elemoff);
+
+        elemHeap.emplace_back(elemoff);
+
+        end = std::chrono::steady_clock::now();
+        //timeTotal += std::chrono::duration<double, timeTtype>(end - start).count();
+
+        //while (elemHeap.capacity() > 0)
+        while (elemHeap.size() > 0)
+        {
+
+            GA_Offset elemHeapLast = elemHeap.last();
+            elemHeap.removeLast();
+
+            attribHandle.set(elemHeapLast, classnum);
+
+            //time(&timeStart);
+            start = std::chrono::steady_clock::now();
+            GA_Size numAdj = geo->getEdgeAdjacentPolygons(adjElems, elemHeapLast);
+            end = std::chrono::steady_clock::now();
+            timeTotal += std::chrono::duration<double, timeTtype>(end - start).count();
+
+            //time(&timeEnd);
+
+            //difftime(timeEnd, timeStart);
+            //GA_Offset* nebs = getPointPointEdgeAdjacent();
+            for (GA_Size i = 0; i < numAdj; ++i)
+            {
+                //GEO_Detail::EdgeAdjacencyData adjElem = adjElems[i];
+                //GA_Offset adjElem = adjElem.myAdjacentPolygon;
+                adjElem = adjElems[i].myAdjacentPolygon;
+                attribValue = attribHandle.get(adjElem);
+                if (attribValue != UNREACHED_NUMBER)
+                    continue;
+                //elemHeap.append(adjElem);
+
+                elemHeap.emplace_back(adjElem);
+
+            }
+        } // after this while loop, elemHeap.size() == 0
+        ++classnum;
+    });
+#else
+    GA_Offset attribValue;
+    GA_Offset elemHeapLast;
     //for (GA_Iterator it(range); it.blockAdvance(start, end); )
-    
     for (GA_Iterator it(range); it.fullBlockAdvance(start, end); )
     {
         for (GA_Offset elemoff = start; elemoff < end; ++elemoff)
@@ -117,7 +171,7 @@ connectivity(
             elemHeap.emplace_back(elemoff);
 
             auto end = std::chrono::steady_clock::now();
-            timeTotal += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+            timeTotal += std::chrono::duration_cast<timeTtype>(end - start).count();
 
             //while (elemHeap.capacity() > 0)
             while (elemHeap.size() > 0)
@@ -128,17 +182,18 @@ connectivity(
                 elemHeap.removeLast();
 
                 end = std::chrono::steady_clock::now();
-                timeTotal += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+                timeTotal += std::chrono::duration_cast<timeTtype>(end - start).count();
 
                 attribHandle.set(elemHeapLast, classnum);
 
                 //time(&timeStart);
-               	numAdj = geo->getEdgeAdjacentPolygons(adjElems, elemHeapLast);
+                GA_Size numAdj = geo->getEdgeAdjacentPolygons(adjElems, elemHeapLast);
 
                 //time(&timeEnd);
                 
                 //difftime(timeEnd, timeStart);
                 //GA_Offset* nebs = getPointPointEdgeAdjacent();
+                start = std::chrono::steady_clock::now();
                 for (GA_Size i = 0; i < numAdj; ++i)
                 {
                     //GEO_Detail::EdgeAdjacencyData adjElem = adjElems[i];
@@ -148,23 +203,36 @@ connectivity(
                     if (attribValue != UNREACHED_NUMBER)
                         continue;
                     //elemHeap.append(adjElem);
-                    start = std::chrono::steady_clock::now();
 
                     elemHeap.emplace_back(adjElem);
 
-                    end = std::chrono::steady_clock::now();
-                    timeTotal += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
                 }
+                end = std::chrono::steady_clock::now();
+                timeTotal += std::chrono::duration_cast<timeTtype>(end - start).count();
             } // after this while loop, elemHeap.size() == 0
             ++classnum;
         }
     }
-    
+#endif
     attribHandle->bumpDataId();
-
+#if 1
+#if 1
+    //GA_Attribute* attribPtrDebug = geo->createAttribute(GA_ATTRIB_DETAIL, "time", nullptr, nullptr, nullptr);
+    //GA_RWHandleT<fpreal> attribHandleDebug(attribPtrDebug);
+    geo->setDetailAttributeF("time", timeTotal);
+    //char *asStr2;
+    //sprintf(asStr2, "%lf", timeTotal);
+    //geo->setDetailAttributeS("timestr", asStr2);
+#else
+    GA_Attribute* attribPtrDebug = geo->addFloatTuple(GA_ATTRIB_GLOBAL, "time", 1, GA_Defaults(-1.0), 0, 0, GA_STORE_REAL64);
+    GA_RWHandleT<fpreal> attribHandleDebug(attribPtrDebug);
+#endif
+#else
     GA_Attribute* attribPtrDebug = geo->addIntTuple(GA_ATTRIB_GLOBAL, "time", 1, GA_Defaults(0), 0, 0, GA_STORE_INT64);
     GA_RWHandleT<exint> attribHandleDebug(attribPtrDebug);
     attribHandleDebug.set(0, timeTotal);
+    attribHandleDebug->bumpDataId();
+#endif
 }
 
 
